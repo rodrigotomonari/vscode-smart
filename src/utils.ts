9d901ab5c45525e0baa2b67360d5caa7bbb79a94
Rodrigo Tomonari <rodrigo.tomonari@smartpension.co.uk>
2022-02-04 10:19:06 +0000

    Fix indent style

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 export function notEmpty<T>(value: T | null | undefined): value is T {
-	return value !== null && value !== undefined;
+  return value !== null && value !== undefined;
 }
 
 export function compareBy<T, K extends keyof T>(key: K) {
-  return function(a: T, b: T) {
+  return function (a: T, b: T) {
     return a[key] > b[key] ? 1 : -1;
   };
 }
